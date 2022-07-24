d4c71a669d601e746dffa8183896cbca837419ef
minsu <choalstn@gmail.com>
2022-07-24 21:06:01 +0900

    변수, 상수, 리터

---
+++ Practice_Chap02(변수)/src/example01/VarEx02.java
@@ -0,0 +1,22 @@
+package example01;
+
+public class VarEx02 {
+
+	public static void main(String[] args) {
+		
+		int number; 
+		number = 200;
+		
+		int number2 = 500;
+		
+		System.out.println(number);
+		System.out.println(number2);
+		
+		String str = new String("Bread");
+		System.out.println(str);
+		str = null;
+		System.out.println(str);
+		
+	}
+
+}
