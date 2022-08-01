369640f0b740e97f3f156c95ea17c10e3d906922
minsu <choalstn@gmail.com>
2022-08-01 21:07:32 +0900

    스캐너

--- Practice_Chap02(변수)/src/example01/ScannerEx.java
+++ Practice_Chap02(변수)/src/example01/ScannerEx.java
@@ -8,20 +8,22 @@ public class ScannerEx {
 		//Ctrl + shift + o == 자동 임포트 
 		//입력을 받기 위한 클래스이고, 입력스트림   
 		Scanner sc = new Scanner(System.in);
-		System.out.print("정수 한 자리를 입력 :  ");
-		int num = sc.nextInt();
-		System.out.println("사용자로부터 입력받은 int :  " + num);
+		//System.out.print("정수 한 자리를 입력 :  ");
+		//int num = sc.nextInt();
+		//System.out.println("사용자로부터 입력받은 int :  " + num);
 		
 		
-		System.out.print("실수 한 자리를 입력 :  ");
-		double dnum = sc.nextDouble();
-		System.out.println("사용자로부터 입력받은 double :  " + dnum);
+		//System.out.print("실수 한 자리를 입력 :  ");
+		//double dnum = sc.nextDouble();
+		//System.out.println("사용자로부터 입력받은 double :  " + dnum);
 		
 		
-		System.out.print("문자열 입력 :  ");
-		//String str = sc.next(); //next() 메소드는 공백을 기준으로 입력 -- 단어 입력  
-		String str = sc.nextLine(); //공백포함, 엔터키를 입력할때까지 입력 받음  -- 문장 입력   
-		System.out.println("사용자로부터 입력받은 string :  " + str);
+		String str1 = sc.next(); //next() 메소드는 공백을 기준으로 입력 -- 단어 입력  
+		sc.nextLine();
+		String str2 = sc.nextLine(); //공백포함, 엔터키를 입력할때까지 입력 받음  -- 문장 입력 
+		
+		System.out.println("next() 메소드 :  " + str1);
+		System.out.println("nextLine() 메소드 :  " + str2);
 		
 		
 		
