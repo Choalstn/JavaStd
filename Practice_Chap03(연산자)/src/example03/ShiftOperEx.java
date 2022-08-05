d3b488f613adce7aae39b2c43014d7d547dd014e
minsu <choalstn@gmail.com>
2022-08-05 18:32:14 +0900

    Operator(3)

---
+++ Practice_Chap03(연산자)/src/example03/ShiftOperEx.java
@@ -0,0 +1,18 @@
+package example03;
+
+public class ShiftOperEx {
+
+	public static void main(String[] args) {
+		
+		int a = 8;
+		int b = 2;
+		
+		//shift연산은 그래픽 분야에서 많이 사용 -> 속도가 빨라서  
+		System.out.println("8 << 2 : " + (a << b));
+		System.out.println("8 >> 2 : " + (a >> b));
+		
+		
+
+	}
+
+}
